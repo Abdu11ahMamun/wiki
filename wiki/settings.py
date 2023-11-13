8928abc4812212c4bc7d692bf36f2660b1473ead
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-11-13 21:30:37 +0600

    url settings and new md page created

--- wiki/settings.py
+++ wiki/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(ROOT_URLCONF, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
