4dd17baebe400581f6c7db04889fee78e80e940c
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-12-04 02:23:14 +0600

    converting md files to html done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
+def convert_md_to_html(title):
+    md_content = util.get_entry(title)
+    if md_content == None:
+        return None
+    else:
+        return markdown2.markdown(md_content)
 
 def index(request):
     content = util.list_entries()
     css = util.get_entry("CSS")
-    nak = util.get_entry("nak")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request, title):
-    content = util.get_entry(title)
 
-    if content is None:
-        return render(request, "encyclopedia/404.html",{"title": title})
-    return render(request, "encyclopedia/entry.html", {"title":title}, )
    
 
